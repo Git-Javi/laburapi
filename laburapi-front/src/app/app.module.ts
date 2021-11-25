42858fa3c783bb7aa5b93ba85a1b3156c3a3c847
Git-Javi <pruebasfpcastelao@gmail.com>
2021-11-25 16:05:19 +0100

    Creados componentes de header, footer y login

--- laburapi-front/src/app/app.module.ts
+++ laburapi-front/src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+/* ########################### COMPONENTES ########################### */
+import { AppComponent } from './components/app-component/app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
